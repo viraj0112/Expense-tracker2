21fdc3078976b47ddf46eb7bbbb5d473c1b907fc
viraj0112 <83603367+viraj0112@users.noreply.github.com>
2022-08-01 22:45:23 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import Expenses from "./Components/Expenses/Expenses";
+import "./App.css";
+import NewExpense from "./Components/NewExpenses/NewExpense";
+const App = () => {
+  const Dummy_expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  // const App = () => {
+  const [expenses, setExpenses] = useState(Dummy_expenses);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement(Expenses, { items: expenses }),
+  //   React.createElement(NewExpense)
+  // ); //(This is the alternate to write JSX code more simple to write.^)
+
+  return (
+    <div>
+      <div className="Title">
+        <center>
+          <h1>Expense Tracker</h1>
+        </center>
+      </div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+  // };
+};
+export default App;
